acf87bd88427cfb757c3d2c8e468d989347e4f69
이상지 <sangji@MacBook-Pro-2.local>
2022-10-01 19:12:05 +0900

    add unittest CI

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def     helloworld(a):
-    print(f"Hello World! {a}")
+def helloworld(a):
+    msg = f'Hello World! {a}'
+    print(msg)
+    return msg
+
+
 if __name__ == "__main__":
-    helloworld("sj..")
+    helloworld("sj")
