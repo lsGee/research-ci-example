08b93e7fa31042d7d31bd64511489d0d1c0c3328
이상지 <sangji@MacBook-Pro-2.local>
2022-10-01 20:15:03 +0900

    test ok

--- main_test.py
+++ main_test.py
@@ -5,8 +5,8 @@ import main  # internal module
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        ret = main.helloworld('Test')
+        ret = main.helloworld('sj')
         self.assertEqual(ret, 'Hello World! sj')
         
 if __name__ == '__main__':
-    unittest.main()  # 실행 시, 유닛테스트 통과 X
+    unittest.main()  # 실행 시, 유닛테스트 통과
