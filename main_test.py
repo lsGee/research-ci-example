29ec6e155c6d28493019c951dc2a36cc967f9eae
이상지 <sangji@MacBook-Pro-2.local>
2022-10-01 20:29:43 +0900

    balck main and main_test

--- main_test.py
+++ main_test.py
@@ -5,8 +5,9 @@ import main  # internal module
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        ret = main.helloworld('sj')
-        self.assertEqual(ret, 'Hello World! sj')
-        
-if __name__ == '__main__':
+        ret = main.helloworld("sj")
+        self.assertEqual(ret, "Hello World! sj")
+
+
+if __name__ == "__main__":
     unittest.main()  # 실행 시, 유닛테스트 통과
