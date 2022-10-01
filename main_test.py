76d371dd97eeacb99f8fad311922472b5ffd25ea
이상지 <sangji@MacBook-Pro-2.local>
2022-10-01 19:15:54 +0900

    add unittest CI

---
+++ main_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+import main  # internal module
+
+
+class MainTest(unittest.TestCase):
+    def test_helloworld(self):
+        ret = main.helloworld('Test')
+        self.assertEqual(ret, 'Hello World! sj')
+        
+if __name__ == '__main__':
+    unittest.main()  # 실행 시, 유닛테스트 통과 X
